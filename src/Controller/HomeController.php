27dfdbed3931d5f1f44bf6bcf3c34bce2e1513bc
Wilder <charlene.mvu@hotmail.fr>
2019-07-16 17:25:47 +0200

    creation controllers et crud

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wilder
+ * Date: 16/07/19
+ * Time: 17:13
+ */
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/home")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function index(){
+            return $this->render('default.html.twig');
+        }
+}
